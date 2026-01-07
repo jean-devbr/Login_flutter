60aee0e0378af308aaa78c110d600c2cbbe1c4c4
jean-devbr <costajean1005@gmail.com>
2026-01-07 14:37:40 -0300

    removendo os arquivos de sprint boot

--- src/main/java/com/uber/login_simples/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.uber.login_simples.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginDto(
-        @NotBlank @Email String email,
-        @NotBlank String senha
-) {}
